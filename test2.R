aad2b8fa811eb17a60341e9c251205572ffeaa3b
rosstelli <ross-donatelli@jmilabs.com>
2018-02-08 15:13:04 -0600

    Attempt at simple package.

---
+++ test2.R
@@ -0,0 +1,13 @@
+install.packages("devtools")
+library("devtools")
+devtools::install_github("klutometis/roxygen")
+library(roxygen2)
+
+setwd("/Users/ross/Desktop/drat_test")
+create("testing")
+
+setwd("/Users/ross/Desktop/drat_test/testing")
+document()
+
+setwd("/Users/ross/Desktop/drat_test")
+install("testing")
