720917f0c85f1b9236b21fafeaa1981b9d2cfc02
rosstelli <ross-donatelli@jmilabs.com>
2018-02-12 13:04:14 -0600

    Updating example documentation

--- testing/R/test_ross.R
+++ testing/R/test_ross.R
@@ -7,6 +7,11 @@
 #' @param y this is another number 
 #' @return Gives back 7.
 #' @export
+#' @examples 
+#' test_ross(
+#' x=2,
+#' y=3
+#' )
 test_ross <- function (x=1,y=2,z=3) {
   return (7);
 }
